73551795d8e5c13b1525a37979f427752587640b
Luciano Zimermann <lucianozn16@gmail.com>
2022-01-12 22:17:06 -0300

    Find movies

---
+++ backend/src/main/java/com/lucianozimermann/dsmovie/repositories/MovieRepository.java
@@ -0,0 +1,8 @@
+package com.lucianozimermann.dsmovie.repositories;
+ 
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.lucianozimermann.dsmovie.entities.Movie;
+
+public interface MovieRepository extends JpaRepository<Movie, Long>{
+}
