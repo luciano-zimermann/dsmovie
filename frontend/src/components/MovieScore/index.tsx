da9c3379626fa940b8df1fc7b18e883841345f22
Luciano Zimermann <lucianozn16@gmail.com>
2022-01-15 00:40:33 -0300

    Adjustment in the rating label when it has 0 or 1 ratings

--- frontend/src/components/MovieScore/index.tsx
+++ frontend/src/components/MovieScore/index.tsx
@@ -6,13 +6,15 @@ type Props = {
     count: number;
 }
 
-function MovieScore({ score, count }: Props) {
+function MovieScore({ score, count } : Props ) {
+
+    var ratingsLabel = count > 1 ? "Avaliações" : "Avaliação";
 
     return (
         <div className="dsmovie-score-container">
             <p className="dsmovie-score-value">{score > 0 ? score.toFixed(1) : '-'}</p>
             <MovieStars score={score} />
-            <p className="dsmovie-score-count">{count} avaliações</p>
+            <p className="dsmovie-score-count">{count} {ratingsLabel}</p>
         </div>
     );
 }
