e23794af3594a65abb377c587783e9fe459f80a9
Luciano Zimermann <lucianozn16@gmail.com>
2022-01-14 23:17:50 -0300

    show score

--- frontend/src/components/MovieCard/index.tsx
+++ frontend/src/components/MovieCard/index.tsx
@@ -1,5 +1,4 @@
 import MovieScore from "components/MovieScore";
-import { type } from "os";
 import { Link } from "react-router-dom";
 import { Movie } from "types/movie";
 
@@ -14,7 +13,7 @@ function MovieCard( { movie } : Props ) {
             <img className="dsmovie-movie-card-image" src={movie.image} alt={movie.title} />
             <div className="dsmovie-card-bottom-container">
                 <h3>{movie.title}</h3>
-                <MovieScore />
+                <MovieScore count={movie.count} score={movie.score} />
 
                 <Link to={`/form/${movie.id}`}>
                     <div className="btn btn-primary dsmovie-btn">Avaliar</div>
